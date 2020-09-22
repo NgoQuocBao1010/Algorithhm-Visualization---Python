4c79b79f59b5f0c2b2aef2054aa3fa817d2dba35
NgoQuocBao1010 <56999031+NgoQuocBao1010@users.noreply.github.com>
2020-09-22 15:54:27 +0700

    21/09/2020

--- depthfirstsearch.py
+++ depthfirstsearch.py
@@ -40,10 +40,19 @@ class Node:
 		self.radius = 40
 		self.borderWidth = 5
 		self.color = BLACK
+		self.edges = []
 		self.neighbours = []
+		self.dragging = False
+		self.clickedPosition = ()
 
+	def isCliked(self, point):
+		p_x, p_y = point
+		self.clickedPosition = (p_x, p_y)
+		distance = math.sqrt((self.x - p_x) * (self.x - p_x) + (self.y - p_y) * (self.y - p_y))
 
-	def getPosition(self):
+		return distance <= self.radius
+
+	def getCenterPosition(self):
 		return self.x, self.y
 
 	def draw(self, win):
@@ -52,6 +61,13 @@ class Node:
 
 	def __str__(self):
 		return self.order
+
+	def reset(self):
+		self.color = BLACK
+
+	def makeDragged(self):
+		self.color = GREEN
+
 
 
 def makeGrid(nodes, width):
@@ -78,25 +94,52 @@ def makeGrid(nodes, width):
 	return grid
 
 
-def draw(win, grid, nodes, width):
+def draw(win, grid, nodes, width, lineConfig):
 	win.fill(WHITE)
+	s_x, s_y, e_x, e_y = lineConfig
+
+	if s_x != 0:
+		pygame.draw.line(win, GREY, (s_x, s_y), (e_x, e_y), 5)
+
 	for node in grid:
 		node.draw(win)
 	pygame.display.update()
+
+
 
 
 def main(win, width):
 	NODES = 10
 	run = True
 	grid = makeGrid(NODES, width)
-
+	s_x = s_y = e_x = e_y = 0
 
 	while run:
-		draw(win, grid, NODES, width)
+		draw(win, grid, NODES, width, (s_x, s_y, e_x, e_y))
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				run = False
 
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				if event.button == 1:
+					mouse_x, mouse_y = event.pos
+					for node in grid:
+						if node.isCliked((mouse_x, mouse_y)):
+							node.dragging = True
+							node.makeDragged()
+			elif event.type == pygame.MOUSEBUTTONUP: 
+				if event.button == 1:
+					for node in grid:
+						if node.dragging:
+							node.dragging = False
+							s_x = s_y = e_x = e_y = 0
+							node.reset()
+			elif event.type == pygame.MOUSEMOTION:
+				for node in grid:
+						if node.dragging:
+							s_x, s_y = node.clickedPosition
+							e_x, e_y = event.pos
+							
 	pygame.quit()
 
 main(WIN, WIDTH)
